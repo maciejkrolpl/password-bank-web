3fe594dfc32e452669a11bc7f914d4aaee876ea4
Maciej Król <37816451+maciejkrolpl@users.noreply.github.com>
2018-06-25 23:18:26 +0200

    Create repos services controllers (#5)
    * Created controllers, services, repos
    
    * Create all basic methods in controller

--- src/main/java/com/github/maciejkrolpl/controller/PasswordEntryController.java
+++ src/main/java/com/github/maciejkrolpl/controller/PasswordEntryController.java
@@ -3,6 +3,10 @@ package com.github.maciejkrolpl.controller;
 import com.github.maciejkrolpl.model.PasswordEntry;
 import com.github.maciejkrolpl.service.PasswordEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +24,24 @@ public class PasswordEntryController {
         this.service = service;
     }
 
+    @GetMapping("/{id}")
+    public PasswordEntry findOne(@PathVariable Long id) {
+        return service.findOneById(id);
+    }
+
     @GetMapping
     public Set<PasswordEntry> findAll() {
         return service.findAll();
+    }
+
+    @PostMapping
+    public PasswordEntry create(PasswordEntry passwordEntry) {
+        return service.createPasswordEntry(passwordEntry);
+    }
+
+    @DeleteMapping
+    public void delete(Long id) {
+        service.deleteOneById(id);
     }
 
 }
