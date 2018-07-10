fffc6baecdbfeba726ba049929fbb168146e3339
pirat21wieka <pirat21wieka@gmail.com>
2018-07-10 23:43:19 +0200

    Index page with all entries

--- src/main/java/com/github/maciejkrolpl/encrypt/EncryptDecrypt.java
+++ src/main/java/com/github/maciejkrolpl/encrypt/EncryptDecrypt.java
@@ -1,5 +1,7 @@
 package com.github.maciejkrolpl.encrypt;
 
+import org.jasypt.util.text.BasicTextEncryptor;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -10,47 +12,18 @@ import java.security.NoSuchAlgorithmException;
 
 public class EncryptDecrypt {
 
-    private static byte[] key = "4da5ea60fd8f7ee69f60fc30504064e0".getBytes();
-
-    public static Byte[] encrypt(String text) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-
-        byte[] dataToSend = text.getBytes();
 
 
-        Cipher c = Cipher.getInstance("AES");
-        SecretKeySpec k =
-                new SecretKeySpec(key, "AES");
-        c.init(Cipher.ENCRYPT_MODE, k);
-
-        byte[] bytes = c.doFinal(dataToSend);
-
-        Byte[] result = new Byte[bytes.length];
-
-        for (int i = 0; i < result.length; i++) {
-            result[i] = bytes[i];
-        }
-
-        return result;
-
+    public static String encrypt(String plainText, String key) {
+        BasicTextEncryptor encryptor = new BasicTextEncryptor();
+        encryptor.setPasswordCharArray(key.toCharArray());
+        return encryptor.encrypt(plainText);
     }
 
-    public static String decrypt(Byte[] encryptedData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-
-
-                Cipher c = Cipher.getInstance("AES");
-        SecretKeySpec k =
-                new SecretKeySpec(key, "AES");
-        c.init(Cipher.DECRYPT_MODE, k);
-
-        byte[] bytes = new byte[encryptedData.length];
-
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = encryptedData[i];
-        }
-
-        byte[] data = c.doFinal(bytes);
-
-        return new String(data);
+    public static String decrypt(String plainText, String key) {
+        BasicTextEncryptor encryptor = new BasicTextEncryptor();
+        encryptor.setPasswordCharArray(key.toCharArray());
+        return encryptor.decrypt(plainText);
     }
 
 }
