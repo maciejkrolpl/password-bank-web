db8473c238703c9a3ca7ab5e7cf4c75fce4ddf51
pirat21wieka <pirat21wieka@gmail.com>
2018-08-03 22:54:18 +0200

    Handling entry editing form

--- src/main/java/com/github/maciejkrolpl/controller/WebController.java
+++ src/main/java/com/github/maciejkrolpl/controller/WebController.java
@@ -1,14 +1,14 @@
 package com.github.maciejkrolpl.controller;
 
+import com.github.maciejkrolpl.dto.PasswordEntryDto;
 import com.github.maciejkrolpl.dto.PasswordEntryHtmlDto;
+import com.github.maciejkrolpl.dto.PasswordEntrySaveDto;
 import com.github.maciejkrolpl.model.PasswordEntry;
 import com.github.maciejkrolpl.service.PasswordEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -41,7 +41,19 @@ public class WebController {
         PasswordEntryHtmlDto dto = new PasswordEntryHtmlDto(entry);
         model.addAttribute("entry", dto);
         return "entry";
+    }
 
+    @RequestMapping(value="/edit/{id}", method = RequestMethod.GET)
+    public String edit(Model model, @PathVariable Long id) {
+        PasswordEntry entry = service.findOneById(id);
+        PasswordEntryHtmlDto dto = new PasswordEntryHtmlDto(entry);
+        model.addAttribute("entry", dto);
+        return "edit";
+    }
+
+    @PostMapping("/submit")
+    public String submit(@ModelAttribute("entry") PasswordEntrySaveDto entry) {
+        return "submit";
     }
 
 }
